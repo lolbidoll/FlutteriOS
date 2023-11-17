8a02902f5ea6d5ba6694a8491aa4b6852f97fd8b
Jobin Kurian <jobin.kurian@netcorecloud.com>
2023-11-17 13:10:08 +0530

    Basic Notification Setup Done for iOS
    We are able to see APNS Token from the native app side and flutter side.

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -10,4 +10,22 @@
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+    NSLog(@"Device Token = %@", [self getDeviceTokenFromData:deviceToken]);
+    [super application:application didRegisterForRemoteNotificationsWithDeviceToken:deviceToken];
+}
+
+- (nullable NSString *)getDeviceTokenFromData:(NSData *)deviceToken {
+    NSUInteger dataLength = deviceToken.length;
+    if (dataLength == 0) {
+        return nil;
+    }
+    const unsigned char *dataBuffer = (const unsigned char *)deviceToken.bytes;
+    NSMutableString *hexString = [NSMutableString stringWithCapacity:(dataLength * 2)];
+    for (int i = 0; i < dataLength; ++i) {
+        [hexString appendFormat:@"%02x", dataBuffer[i]];
+    }
+    return [hexString copy];
+}
+
 @end
--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,75 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await configureFirebase();
+  runApp(const MyApp());
+}
+
+Future<void> configureFirebase() async {
+  // Initialize Firebase App
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   // Initialize FCM
   FirebaseMessaging messaging = FirebaseMessaging.instance;
 
-  // Request permission to receive notifications
-  // Request permission to receive notifications
-  final NotificationSettings notificationSettings =
-      await messaging.requestPermission(
-    provisional: true,
+  // Use the returned token to send messages to users from your custom server
+  final apnsToken = await FirebaseMessaging.instance.getAPNSToken();
+  if (apnsToken != null) {
+    // APNS token is available, make FCM plugin API requests...
+    print('APNS Token: $apnsToken');
+    final fcmToken = await FirebaseMessaging.instance.getToken();
+    print('FCM Get Token: $fcmToken');
+  } else {
+    print('APNS Token: is null');
+  }
+
+  // Setting up the notification permissio alert;
+  NotificationSettings settings = await messaging.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
   );
 
-  runApp(const MyApp());
+  if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+    print('User granted permission');
+  } else if (settings.authorizationStatus == AuthorizationStatus.provisional) {
+    print('User granted provisional permission');
+  } else {
+    print('User declined or has not accepted permission');
+  }
+
+  await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+      alert: true, badge: true, sound: true);
+
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    print(
+        'Got the notification when the App is in foregorund: [FirebaseMessaging.onMessage.listen]');
+    print('Message data: ${message.data}');
+    if (message.notification != null) {
+      print(
+          'Message also contained a notification: ${message.notification!.title}');
+    }
+  });
+
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+}
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  // If you're going to use other Firebase services in the background, such as Firestore,
+  // make sure you call `initializeApp` before using other Firebase services.
+  // await Firebase.initializeApp();
+
+  print("Handling a background message: ${message.messageId}");
 }
 
 class MyApp extends StatelessWidget {
